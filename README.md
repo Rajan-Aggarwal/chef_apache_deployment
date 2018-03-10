c3e392ee039eeb0b2ca43f7f1d55bfc75f93cf44
vagrant <vagrant@localhost.localdomain>
2018-03-10 11:50:55 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_apache_deployment
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_apache_deployment
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
