c7b6dbb67d5a5d196bbf10592283770abb78414d
Rajan-Aggarwal <rajansien@gmail.com>
2018-03-11 06:41:03 +0000

    Include server recipe in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,5 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe "chef_apache_deployment::server"
--- recipes/server.rb
+++ recipes/server.rb
@@ -3,3 +3,17 @@
 # Recipe:: server
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd' do
+	action :install
+end
+
+file '/var/www/html/index.html' do
+	content '<h1>Hello, apache!</h1>'
+end
+
+service 'httpd' do
+	action [ :enable, :start ]
+end
+
+
